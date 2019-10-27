3d545c840c98738bd3da4ef12b81ec81e091dd6a
rileytwo <34988536+rileytwo@users.noreply.github.com>
2019-10-27 14:31:31 -0500

    Initial Commit

---
+++ Advanced Clock/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Advanced Clock
+//
+//  Created by Riley Roach on 10/27/19.
+//  Copyright © 2019 Riley Roach. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Advanced Clock/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Advanced Clock
+//
+//  Created by Riley Roach on 10/27/19.
+//  Copyright © 2019 Riley Roach. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
